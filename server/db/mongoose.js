715ab022834c2c1e4e7a88016868f0c41a249fca
pratikkhetre <84448502+pratikkhetre@users.noreply.github.com>
2024-04-28 22:58:48 +0530

    Update mongoose.js

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 mongoose.Promise = global.Promise
-const connectionURI = "mongodb+srv://psk123:psk123@stackuptest.9namfg2.mongodb.net/OrganProject" 
+const connectionURI = "" 
 mongoose.connect(connectionURI, 
     {
       useNewUrlParser: true, 
